bb0b838bad325da78cade49d9946cf3c5313ef49
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-10-03 00:58:37 -0400

    working version 1.0

---
+++ src/components/titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titles = (props) =>{
+  return (
+    <div>
+      <h1>Weather Forecaster</h1>
+      <p>Find weather conditions around the globe</p>
+    </div>
+  )
+}
+export default Titles;
